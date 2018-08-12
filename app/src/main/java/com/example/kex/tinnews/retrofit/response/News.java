7b392f06d7b3d5c164e92f8b8aa7854bacb41642
Ke Xu <kex@unr.edu>
2018-08-12 15:49:45 -0700

    add tin news card

---
+++ app/src/main/java/com/example/kex/tinnews/retrofit/response/News.java
@@ -0,0 +1,66 @@
+package com.example.kex.tinnews.retrofit.response;
+
+import android.support.annotation.NonNull;
+
+public class News {
+    public String author;
+    @NonNull
+    public String title;
+    public String description;
+    public String url;
+    public String image;
+    public String time;
+
+    public News() {
+
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @NonNull
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(@NonNull String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
